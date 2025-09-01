c7c1b540a80e96a4d187d0655c41b1b50a22ca33
Dragos Daian <daian.dragos@yahoo.com>
2025-09-01 09:49:10 +0200

    upd build only in editor

--- modules/godot_ide/config.py
+++ modules/godot_ide/config.py
@@ -1,7 +1,6 @@
 def can_build(env, platform):
     # Only build this module for editor builds
     target = env.get("target", "")
-    print(f"GodotIDE can_build check: target={target}, platform={platform}")
     if target == "editor":
         env.module_add_dependencies("godot_ide", ["godot_wry"], True)
         return True
